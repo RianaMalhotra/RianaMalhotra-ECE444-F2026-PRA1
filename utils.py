3164f2146cadb5dd9200cca83f436def431e8855
Riana Malhotra <113440635+RianaMalhotra@users.noreply.github.com>
2026-09-14 14:42:36 -0400

    Update utils.py
    Added an if statement that makes sure inputs are only integers

--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
 class utils:
 
     def reversed(self, number):
+        if not isinstaance(number,int):
+            raise TypeErro("Input must be an integer")
         return int(str(number)[::-1]) 
 
     def formatter(self, number):
+         if not isinstance(number, int):
+            raise TypeError("Input must be an integer")
         return bin(int(number)), oct(int(number))
+
