51ed952671bcc8c2a385db71fe52553eaa52fcb7
Riana Malhotra <malhotrariana@gmail.com>
2026-09-14 19:37:30 -0400

    edited test for utils

--- utils_test.py
+++ utils_test.py
@@ -4,27 +4,30 @@ from utils import utils
 
 class TestUtils(unittest.TestCase):
 
-    def test_reversed_string(self):
+    def test_reversed_integers(self):
+        self.assertEqual(utils.reversed(12345), 54321)
+        self.assertEqual(utils.reversed(100), 1)
+        self.assertEqual(utils.reversed(-123), -321)
+
+    def test_reversed_strings(self):
         with self.assertRaises(TypeError):
-            utils().reversed("123")
+            utils.reversed("12345")
 
-    def test_reversed_float(self):
+    def test_reversed_floats(self):
         with self.assertRaises(TypeError):
-            utils().reversed(12.5)
+            utils.reversed(123.45)
 
-    def test_reversed_integer(self):
-        self.assertEqual(utils().reversed(123), 321)
+    def test_formatter_integers(self):
+        self.assertEqual(utils.formatter(10), ("0b1010", "0o12"))
+        self.assertEqual(utils.formatter(8), ("0b1000", "0o10"))
 
-    def test_formatter_string(self):
+    def test_formatter_strings(self):
         with self.assertRaises(TypeError):
-            utils().formatter("10")
+            utils.formatter("10")
 
-    def test_formatter_float(self):
+    def test_formatter_floats(self):
         with self.assertRaises(TypeError):
-            utils().formatter(10.5)
-
-    def test_formatter_integer(self):
-        self.assertEqual(utils().formatter(10), ("0b1010", "0o12"))
+            utils.formatter(10.5)
 
 
 if __name__ == "__main__":
