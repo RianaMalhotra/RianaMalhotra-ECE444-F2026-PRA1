7db6ea86801e912a89ec7beb449e2638bae88b5a
Riana Malhotra <malhotrariana@gmail.com>
2026-09-14 14:47:14 -0400

    included the error if not an integer to the test from utils class

--- utils_test.py
+++ utils_test.py
@@ -5,22 +5,23 @@ from utils import utils
 class TestUtils(unittest.TestCase):
 
     def test_reversed_string(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(TypeError):
             utils().reversed("123")
 
     def test_reversed_float(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(TypeError):
             utils().reversed(12.5)
 
     def test_reversed_integer(self):
         self.assertEqual(utils().reversed(123), 321)
 
     def test_formatter_string(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(TypeError):
             utils().formatter("10")
 
     def test_formatter_float(self):
-        self.assertEqual(utils().formatter(10.5), ("0b1010", "0o12"))
+        with self.assertRaises(TypeError):
+            utils().formatter(10.5)
 
     def test_formatter_integer(self):
         self.assertEqual(utils().formatter(10), ("0b1010", "0o12"))
